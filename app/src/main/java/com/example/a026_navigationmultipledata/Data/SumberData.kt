09dd5a9229b12c33a4198e7781b06bb7815a3920
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-11-16 11:29:33 +0700

    Menambahkan sumber data

---
+++ app/src/main/java/com/example/a026_navigationmultipledata/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.a026_navigationmultipledata.Data
+
+import com.example.a026_navigationmultipledata.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
