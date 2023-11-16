769e0af78d20aae21163202ad6037eb343b67e7c
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-11-16 11:27:10 +0700

    Menambahkan data class

---
+++ app/src/main/java/com/example/a026_navigationmultipledata/Data/PociUIState.kt
@@ -0,0 +1,10 @@
+package com.example.a026_navigationmultipledata.Data
+
+data class PociUIState(
+    val nama: String = "",
+    val noHP: String = "",
+    val alamat: String = "",
+    val jumlah: String = "",
+    val rasa: String = "",
+    val harga: String = "",
+)
